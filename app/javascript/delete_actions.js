dcc8e6a95a3ed57aee31e66be5fb7193207e7bef
AG0811 <ag114126+d@gmail.com>
2024-06-20 15:26:23 +0900

    いいねで画像表示

--- app/javascript/delete_actions.js
+++ app/javascript/delete_actions.js
@@ -1,22 +1,16 @@
 document.addEventListener('turbo:load', function() {
-    // 削除ボタンのイベントをキャプチャし、ログを表示してからイベントの伝播を停止
-  // const deleteForms = document.querySelectorAll('form.button_to');
-  // deleteForms.forEach(form => {
-  //   const confirmMessage = "注：記事を消去しますか？";
-  //   form.addEventListener('submit', (event) => {
-  //     if (!confirm(confirmMessage)) {
-  //       event.stopPropagation();
-  //       event.preventDefault();
-  //     }
-  //   });
-  // });
   const deleteButtons = document.querySelectorAll('form.button_to button');
+
   deleteButtons.forEach(button => {
     button.addEventListener('click', (event) => {
       event.stopPropagation();
       const confirmMessage = "注：記事を消去しますか？";
-      if (!confirm(confirmMessage)) {
-        event.preventDefault();
+
+      // 特定のボタン（例: .delete-buttonクラスを持つボタン）の場合の処理
+      if (button.classList.contains('show-delete')) {
+        if (!confirm(confirmMessage)) {
+          event.preventDefault();
+        }
       }
     });
   });
