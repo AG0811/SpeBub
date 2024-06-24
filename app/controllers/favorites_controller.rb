10c14ca54eb053844814652d5403c79836405a4a
AG0811 <ag114126+d@gmail.com>
2024-06-24 17:17:27 +0900

    いいね修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,23 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
 
   def create
-    news = News.find(params[:news_id])
-    current_user.favorite_news << news
-    redirect_to root_path, notice: 'いいねしました。'
+    @news = News.find(params[:news_id])
+    if current_user.favorite_news.include?(@news)
+      redirect_to root_path, alert: 'すでにいいね済みです。'
+    else
+      current_user.favorite_news << @news
+      redirect_to root_path, notice: 'いいねしました。'
+    end
   end
 
   def destroy
-    news = News.find(params[:news_id])
-    current_user.favorite_news.delete(news)
-    redirect_to root_path, notice: 'いいねを取り消しました。'
+    @news = News.find(params[:news_id])
+    if current_user.favorite_news.include?(@news)
+      current_user.favorite_news.delete(@news)
+      redirect_to root_path, notice: 'いいねを取り消しました。'
+    else
+      redirect_to root_path, alert: 'いいねしていません。'
+    end
   end
 end
