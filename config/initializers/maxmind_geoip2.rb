6329c8671fa727a6b48c79917fad88ef0be754df
AG0811 <ag114126+d@gmail.com>
2024-07-01 17:59:27 +0900

    修正

--- config/initializers/maxmind_geoip2.rb
+++ config/initializers/maxmind_geoip2.rb
@@ -1,12 +1,4 @@
-# # config/initializers/maxmind_geoip2.rb
-# require 'maxmind/geoip2'
 
-# # ライセンスキーとアカウントIDを設定する
-# MaxMind::GeoIP2.configure do |config|
-#   config.account_id  = '1032741'
-#   config.license_key = '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'
-#   config.logger      = Rails.logger
-# end
 
 
 
